82f05a37d5a1ea13ebce5512e9689dfc13ac5287
Guilherme Rodrigues <guilherme.rp2010@hotmail.com>
2021-12-22 09:50:48 -0300

    refactor: seeds passed to shared folder

--- src/pages/Game/viewModels/gameViewModel.ts
+++ src/pages/Game/viewModels/gameViewModel.ts
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from "react";
+import { Seeds } from '../../../shared/seeds';
 
 export interface IUseGameViewModel {
   onClickRandomBtn: (e: React.MouseEvent<HTMLButtonElement>) => void,
@@ -19,7 +20,7 @@ export const useGameViewModel = (): IUseGameViewModel => {
   const [gridLength, setGridLength] = useState(50);
   const [ticksInterval, setTicksInterval] = useState(null);
 
-  const [generationSpeed, setGenerationSpeed] = useState('Fast');
+  const [generationSpeed, setGenerationSpeed] = useState(0.2);
 
   const _generateMatrix = () => {
     const matrixArray = [];
@@ -44,157 +45,7 @@ export const useGameViewModel = (): IUseGameViewModel => {
 
     const matrixCopy = [...matrix];
 
-    // Random
-    // for (let row = 0; row < matrixCopy.length; row++) {
-    //   const matrixCopyRow = matrixCopy[row];
-    //   for (let column = 0; column < matrixCopyRow.length; column++) {
-    //     const randomValue = Math.floor(Math.random() * 2);
-    //     matrixCopy[row][column] = randomValue;
-    //   }
-    // }
-
-    // Beehive
-    // matrixCopy[20][20] = 1;
-    // matrixCopy[20][21] = 1;
-    // matrixCopy[21][19] = 1;
-    // matrixCopy[21][22] = 1;
-    // matrixCopy[22][20] = 1;
-    // matrixCopy[22][21] = 1;
-
-    // Blinker Oscillator
-    // matrixCopy[20][20] = 1;
-    // matrixCopy[20][21] = 1;
-    // matrixCopy[20][22] = 1;
-
-    // Beacon
-    // matrixCopy[20][20] = 1;
-    // matrixCopy[20][21] = 1;
-    // matrixCopy[21][20] = 1;
-    // matrixCopy[22][23] = 1;
-    // matrixCopy[23][22] = 1;
-    // matrixCopy[23][23] = 1;
-
-    // Glider
-    // matrixCopy[20][20] = 1;
-    // matrixCopy[21][21] = 1;
-    // matrixCopy[22][19] = 1;
-    // matrixCopy[22][20] = 1;
-    // matrixCopy[22][21] = 1;
-
-    // Small Exploder
-    // matrixCopy[20][20] = 1;
-    // matrixCopy[21][19] = 1;
-    // matrixCopy[21][20] = 1;
-    // matrixCopy[21][21] = 1;
-    // matrixCopy[22][19] = 1;
-    // matrixCopy[22][21] = 1;
-    // matrixCopy[23][20] = 1;
-
-    // Exploder
-    // matrixCopy[20][18] = 1;
-    // matrixCopy[20][20] = 1;
-    // matrixCopy[20][22] = 1;
-    // matrixCopy[21][18] = 1;
-    // matrixCopy[21][22] = 1;
-    // matrixCopy[22][18] = 1;
-    // matrixCopy[22][22] = 1;
-    // matrixCopy[23][18] = 1;
-    // matrixCopy[23][22] = 1;
-    // matrixCopy[24][18] = 1;
-    // matrixCopy[24][20] = 1;
-    // matrixCopy[24][22] = 1;
-
-    // 10 cell row
-    // matrixCopy[20][15] = 1;
-    // matrixCopy[20][16] = 1;
-    // matrixCopy[20][17] = 1;
-    // matrixCopy[20][18] = 1;
-    // matrixCopy[20][19] = 1;
-    // matrixCopy[20][20] = 1;
-    // matrixCopy[20][21] = 1;
-    // matrixCopy[20][22] = 1;
-    // matrixCopy[20][23] = 1;
-    // matrixCopy[20][24] = 1;
-
-    // Lightweight spaceship
-    // matrixCopy[20][15] = 1;
-    // matrixCopy[20][16] = 1;
-    // matrixCopy[20][17] = 1;
-    // matrixCopy[20][18] = 1;
-    // matrixCopy[21][14] = 1;
-    // matrixCopy[21][18] = 1;
-    // matrixCopy[22][18] = 1;
-    // matrixCopy[23][15] = 1;
-    // matrixCopy[23][17] = 1;
-
-    // Tumbler
-    // matrixCopy[20][18] = 1;
-    // matrixCopy[20][19] = 1;
-    // matrixCopy[20][21] = 1;
-    // matrixCopy[20][22] = 1;
-    // matrixCopy[21][18] = 1;
-    // matrixCopy[21][19] = 1;
-    // matrixCopy[21][21] = 1;
-    // matrixCopy[21][22] = 1;
-    // matrixCopy[22][19] = 1;
-    // matrixCopy[22][21] = 1;
-    // matrixCopy[23][17] = 1;
-    // matrixCopy[23][19] = 1;
-    // matrixCopy[23][21] = 1;
-    // matrixCopy[23][23] = 1;
-    // matrixCopy[24][17] = 1;
-    // matrixCopy[24][19] = 1;
-    // matrixCopy[24][21] = 1;
-    // matrixCopy[24][23] = 1;
-    // matrixCopy[25][17] = 1;
-    // matrixCopy[25][18] = 1;
-    // matrixCopy[25][22] = 1;
-    // matrixCopy[25][23] = 1;
-
-    // Gosper Glider Gun
-    matrixCopy[20][10] = 1;
-    matrixCopy[20][11] = 1;
-    matrixCopy[21][10] = 1;
-    matrixCopy[21][11] = 1;
-
-    matrixCopy[20][19] = 1;
-    matrixCopy[20][20] = 1;
-    matrixCopy[21][18] = 1;
-    matrixCopy[21][20] = 1;
-    matrixCopy[22][18] = 1;
-    matrixCopy[22][19] = 1;
-
-    matrixCopy[22][26] = 1;
-    matrixCopy[22][27] = 1;
-    matrixCopy[23][26] = 1;
-    matrixCopy[23][28] = 1;
-    matrixCopy[24][26] = 1;
-
-    matrixCopy[20][32] = 1;
-    matrixCopy[20][33] = 1;
-    matrixCopy[19][32] = 1;
-    matrixCopy[19][34] = 1;
-    matrixCopy[18][33] = 1;
-    matrixCopy[18][34] = 1;
-
-    matrixCopy[30][34] = 1;
-    matrixCopy[30][35] = 1;
-    matrixCopy[30][36] = 1;
-    matrixCopy[31][34] = 1;
-    matrixCopy[32][35] = 1;
-
-    matrixCopy[19][44] = 1;
-    matrixCopy[19][45] = 1;
-    matrixCopy[18][44] = 1;
-    matrixCopy[18][45] = 1;
-
-    matrixCopy[25][45] = 1;
-    matrixCopy[25][46] = 1;
-    matrixCopy[26][45] = 1;
-    matrixCopy[26][47] = 1;
-    matrixCopy[27][45] = 1;
-
-
+    Seeds["GosperGliderGun"](matrixCopy);
 
     matrixRef.current = matrixCopy;
     setMatrix(matrixCopy);
@@ -205,7 +56,7 @@ export const useGameViewModel = (): IUseGameViewModel => {
 
     if (ticksInterval !== null) return;
 
-    const ticks = setInterval(() => _verifyPopulationAndUpdateIt(), generationSpeed === 'Slow' ? 1 * 1000 : 0.02 * 1000);
+    const ticks = setInterval(() => _verifyPopulationAndUpdateIt(), generationSpeed * 1000);
     setTicksInterval(ticks);
   };
 
---
+++ src/shared/seeds.ts
@@ -0,0 +1,175 @@
+const Random = (matrixCopy: Number[][]) => {
+  for (let row = 0; row < matrixCopy.length; row++) {
+    const matrixCopyRow = matrixCopy[row];
+    for (let column = 0; column < matrixCopyRow.length; column++) {
+      const randomValue = Math.floor(Math.random() * 2);
+      matrixCopy[row][column] = randomValue;
+    }
+  }
+};
+
+const Beehive = (matrixCopy: Number[][]) => {
+  matrixCopy[20][20] = 1;
+  matrixCopy[20][21] = 1;
+  matrixCopy[21][19] = 1;
+  matrixCopy[21][22] = 1;
+  matrixCopy[22][20] = 1;
+  matrixCopy[22][21] = 1;
+};
+
+const BlinkerOscillator = (matrixCopy: Number[][]) => {
+  matrixCopy[20][20] = 1;
+  matrixCopy[20][21] = 1;
+  matrixCopy[20][22] = 1;
+};
+
+const Beacon = (matrixCopy: Number[][]) => {
+  matrixCopy[20][20] = 1;
+  matrixCopy[20][21] = 1;
+  matrixCopy[21][20] = 1;
+  matrixCopy[22][23] = 1;
+  matrixCopy[23][22] = 1;
+  matrixCopy[23][23] = 1;
+};
+
+const Glider = (matrixCopy: Number[][]) => {
+  matrixCopy[20][20] = 1;
+  matrixCopy[21][21] = 1;
+  matrixCopy[22][19] = 1;
+  matrixCopy[22][20] = 1;
+  matrixCopy[22][21] = 1;
+};
+
+const SmallExploder = (matrixCopy: Number[][]) => {  
+  matrixCopy[20][20] = 1;
+  matrixCopy[21][19] = 1;
+  matrixCopy[21][20] = 1;
+  matrixCopy[21][21] = 1;
+  matrixCopy[22][19] = 1;
+  matrixCopy[22][21] = 1;
+  matrixCopy[23][20] = 1;
+};
+
+const Exploder = (matrixCopy: Number[][]) => {    
+  matrixCopy[20][18] = 1;
+  matrixCopy[20][20] = 1;
+  matrixCopy[20][22] = 1;
+  matrixCopy[21][18] = 1;
+  matrixCopy[21][22] = 1;
+  matrixCopy[22][18] = 1;
+  matrixCopy[22][22] = 1;
+  matrixCopy[23][18] = 1;
+  matrixCopy[23][22] = 1;
+  matrixCopy[24][18] = 1;
+  matrixCopy[24][20] = 1;
+  matrixCopy[24][22] = 1;
+};
+
+const TenCellRow = (matrixCopy: Number[][]) => {    
+  matrixCopy[20][15] = 1;
+  matrixCopy[20][16] = 1;
+  matrixCopy[20][17] = 1;
+  matrixCopy[20][18] = 1;
+  matrixCopy[20][19] = 1;
+  matrixCopy[20][20] = 1;
+  matrixCopy[20][21] = 1;
+  matrixCopy[20][22] = 1;
+  matrixCopy[20][23] = 1;
+  matrixCopy[20][24] = 1;
+};
+
+const LightweightSpaceship = (matrixCopy: Number[][]) => {    
+  matrixCopy[20][15] = 1;
+  matrixCopy[20][16] = 1;
+  matrixCopy[20][17] = 1;
+  matrixCopy[20][18] = 1;
+  matrixCopy[21][14] = 1;
+  matrixCopy[21][18] = 1;
+  matrixCopy[22][18] = 1;
+  matrixCopy[23][15] = 1;
+  matrixCopy[23][17] = 1;
+};
+
+const Tumbler = (matrixCopy: Number[][]) => {    
+  matrixCopy[20][18] = 1;
+  matrixCopy[20][19] = 1;
+  matrixCopy[20][21] = 1;
+  matrixCopy[20][22] = 1;
+  matrixCopy[21][18] = 1;
+  matrixCopy[21][19] = 1;
+  matrixCopy[21][21] = 1;
+  matrixCopy[21][22] = 1;
+  matrixCopy[22][19] = 1;
+  matrixCopy[22][21] = 1;
+  matrixCopy[23][17] = 1;
+  matrixCopy[23][19] = 1;
+  matrixCopy[23][21] = 1;
+  matrixCopy[23][23] = 1;
+  matrixCopy[24][17] = 1;
+  matrixCopy[24][19] = 1;
+  matrixCopy[24][21] = 1;
+  matrixCopy[24][23] = 1;
+  matrixCopy[25][17] = 1;
+  matrixCopy[25][18] = 1;
+  matrixCopy[25][22] = 1;
+  matrixCopy[25][23] = 1;
+};
+
+const GosperGliderGun = (matrixCopy: Number[][]) => {
+  matrixCopy[20][10] = 1;
+  matrixCopy[20][11] = 1;
+  matrixCopy[21][10] = 1;
+  matrixCopy[21][11] = 1;
+
+  matrixCopy[20][19] = 1;
+  matrixCopy[20][20] = 1;
+  matrixCopy[21][18] = 1;
+  matrixCopy[21][20] = 1;
+  matrixCopy[22][18] = 1;
+  matrixCopy[22][19] = 1;
+
+  matrixCopy[22][26] = 1;
+  matrixCopy[22][27] = 1;
+  matrixCopy[23][26] = 1;
+  matrixCopy[23][28] = 1;
+  matrixCopy[24][26] = 1;
+
+  matrixCopy[20][32] = 1;
+  matrixCopy[20][33] = 1;
+  matrixCopy[19][32] = 1;
+  matrixCopy[19][34] = 1;
+  matrixCopy[18][33] = 1;
+  matrixCopy[18][34] = 1;
+
+  matrixCopy[30][34] = 1;
+  matrixCopy[30][35] = 1;
+  matrixCopy[30][36] = 1;
+  matrixCopy[31][34] = 1;
+  matrixCopy[32][35] = 1;
+
+  matrixCopy[19][44] = 1;
+  matrixCopy[19][45] = 1;
+  matrixCopy[18][44] = 1;
+  matrixCopy[18][45] = 1;
+
+  matrixCopy[25][45] = 1;
+  matrixCopy[25][46] = 1;
+  matrixCopy[26][45] = 1;
+  matrixCopy[26][47] = 1;
+  matrixCopy[27][45] = 1;
+};
+
+
+export const Seeds = {  
+  "Random": (matrixCopy: Number[][]) => Random(matrixCopy),
+  "Beehive": (matrixCopy: Number[][]) => Beehive(matrixCopy),
+  "BlinkerOscillator": (matrixCopy: Number[][]) => BlinkerOscillator(matrixCopy),
+  "Beacon": (matrixCopy: Number[][]) => Beacon(matrixCopy),
+  "Glider": (matrixCopy: Number[][]) => Glider(matrixCopy),
+  "SmallExploder": (matrixCopy: Number[][]) => SmallExploder(matrixCopy),
+  "Exploder": (matrixCopy: Number[][]) => Exploder(matrixCopy),
+  "TenCellRow": (matrixCopy: Number[][]) => TenCellRow(matrixCopy),
+  "LightweightSpaceship": (matrixCopy: Number[][]) => LightweightSpaceship(matrixCopy),
+  "Tumbler": (matrixCopy: Number[][]) => Tumbler(matrixCopy),
+  "GosperGliderGun": (matrixCopy: Number[][]) => GosperGliderGun(matrixCopy),
+};
