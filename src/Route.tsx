e64bcf54930e79d9af01d7a90e54d43d499add7f
Guilherme Rodrigues <guilherme.rp2010@hotmail.com>
2021-12-28 15:22:43 -0300

    refactor: basename differentiating between prod and dev environment

--- src/Route.tsx
+++ src/Route.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { Game } from './pages/Game';
 
 export const AppRoute = () => {
+
+  const BASENAME = process.env.NODE_ENV === 'development' ? '/' : '/Conway-Game-Of-Life-React'; // because of github pages
+
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={BASENAME}>
       <Routes>
         <Route path="/" element={<Game />} />
       </Routes>
